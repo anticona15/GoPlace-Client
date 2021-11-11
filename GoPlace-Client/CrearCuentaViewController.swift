d3287b5e658fb8ead1360120a37076adad2c2fda
Domininque <dominique.perhu@gmail.com>
2021-11-10 23:32:21 -0500

    initial commit

---
+++ GoPlace-Client/CrearCuentaViewController.swift
@@ -0,0 +1,14 @@
+//
+//  CrearCuentaViewController.swift
+//  GoPlace-Client
+//
+//  Created by Marcelo on 8/11/21.
+
+
+import UIKit
+
+class CrearCuentaViewController: UIViewController {
+    
+    
+    
+}
---
+++ GoPlace-Client/RecuperarCuentaViewController.swift
@@ -0,0 +1,31 @@
+//
+//  RecuperarCuentaViewController.swift
+//  GoPlace-Client
+//
+//  Created by Marcelo on 8/11/21.
+//
+import UIKit
+
+class RecuperarCuentaViewController: UIViewController {
+    
+    
+    @IBAction func btnBack(_ sender: Any) {
+        self.navigationController?.popToRootViewController(animated: true)
+    }
+    
+        override func viewDidLoad() {
+            super.viewDidLoad()
+    }
+    
+    }
+
+    
+    
+    
+
+    
+    
+    
+    
+    
+
